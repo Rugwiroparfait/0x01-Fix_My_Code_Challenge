f4195a77675d74bde25c8fa6dd5ff76bc76ad121
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-05-28 13:45:25 -0700

    square.py

--- square.py
+++ square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        """ Perimeter of the square """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
@@ -26,3 +26,4 @@ if __name__ == "__main__":
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
+
